4ecbbedeea4b39966643e7dfba679820ed44411b
angel <angel@host.docker.internal>
2022-01-23 15:57:17 -0300

    cap04-item02 - Teste de estado de persistência no banco de dados

--- src/test/java/br/com/alura/loja/modelo/TestProduto.java
+++ src/test/java/br/com/alura/loja/modelo/TestProduto.java
@@ -9,12 +9,25 @@ import org.junit.Test;
 import br.com.alura.loja.util.JPAUtil;
 
 public class TestProduto {
+	
+	EntityManager em = JPAUtil.getEntityManager();
+	
 	@Test
 	public void testeInserePorduto() {
-		EntityManager em = JPAUtil.getEntityManager();
+		Categoria computadores = new Categoria("COMPUTADOR");
+//		Produto computador = new Produto("Computador", "Samsung", new BigDecimal(1500), computadores);
 		
-//		em.persist(new Produto("Computador", "Samsung", new BigDecimal(1500), Categoria));
 		
+		em.getTransaction().begin();
+		// Na execução da linha abaixo o objeto computadores fica sendo gerenciado pelo banco de dados
+		em.persist(computadores);
+		// Abaixo a propriedade do nome é alterada pelo banco de dados após o commit
+		computadores.setNome("XPTO");
+//		em.flush();
+		em.getTransaction().commit();
+//		computadores.setNome("XPTO2");
+		
+		em.close();
 	
 	}
 

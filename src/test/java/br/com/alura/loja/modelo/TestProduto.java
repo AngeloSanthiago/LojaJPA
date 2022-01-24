19fcf8d88d60e092d32588166560dad5388164d0
angel <angel@host.docker.internal>
2022-01-24 16:50:40 -0300

    atualização

--- src/test/java/br/com/alura/loja/modelo/TestProduto.java
+++ src/test/java/br/com/alura/loja/modelo/TestProduto.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 
 import org.junit.Test;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.util.JPAUtil;
 
 public class TestProduto {
@@ -76,12 +77,14 @@ public class TestProduto {
 	@Test
 	public void testeInsereUmaVezEAtualizaUmaVezProduto() {
 		Categoria computadores = new Categoria("COMPUTADOR");
+		CategoriaDao compDao = new CategoriaDao(em);
 //		Produto computador = new Produto("Computador", "Samsung", new BigDecimal(1500), computadores);
 		
 		
 		em.getTransaction().begin();
 		// Na execução da linha abaixo o objeto computadores fica sendo gerenciado pelo banco de dados
-		em.persist(computadores);
+//		em.persist(computadores);
+		compDao.cadastrar(computadores);
 		// Abaixo a propriedade do nome é alterada pelo banco de dados após o commit
 		computadores.setNome("XPTO");
 		em.flush();
